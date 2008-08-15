d61cef3df0491bd572f8f3fb7b38a931386d5933
Peter Baumgartner <pete@lincolnloop.com>
2008-08-15 00:27:15 -0600

    load_data bugs and handling pycountry exceptions

--- locations/utils/load_data.py
+++ locations/utils/load_data.py
@@ -1,6 +1,6 @@
 import pycountry
 
-from locations.models import Country, SubCountry
+from locations.models import Country, SubDivision, SubDivisionType
 
 def create_subdivision(subdivision, country):
     """
@@ -10,28 +10,36 @@ def create_subdivision(subdivision, country):
     subdivision is a pycountry object
     country is a Django object
     """
-    t = SubDivisionType.objects.get_or_create(name=subdivision.type)
-    if subdivision.parent_code:
-        parent = subdivision.parent
-        p = create_subdivision(parent)
-    else:
-        p = None
+    t, created = SubDivisionType.objects.get_or_create(name=subdivision.type)
     sd, created = SubDivision.objects.get_or_create(code=subdivision.code,
                                                     country=country,
                                                     name=subdivision.name,
-                                                    sub_division_type=t,
-                                                    parent=p)
+                                                    sub_division_type=t)
+    if subdivision.parent_code:
+        parent = subdivision.parent
+        p = create_subdivision(parent, country)
+        sd.parent = p
+        sd.save()
+
     return sd
     
 
 for country in pycountry.countries.objects:
     c, created = Country.objects.get_or_create(alpha2=country.alpha2,
-                                               alpha2=country.alpha3,
+                                               alpha3=country.alpha3,
                                                name=country.name,
                                                numeric=country.numeric,
-                                               official_name=country.official_name,
                                                published=True)
+    try:
+        c.official_name=country.official_name
+        c.save()
+    except AttributeError:
+        pass
 
-    for subdivision in pycountry.subdivisions.get(country_code=c.alpha2):
-        create_subdivision(subdivision)
+    try:
+        subdivisions = pycountry.subdivisions.get(country_code=c.alpha2)
+    except KeyError:
+        subdivisions = []
+    for subdivision in subdivisions:
+        create_subdivision(subdivision, c)
         
