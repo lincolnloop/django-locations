b35205fcad8d10860bb0d51efe12c9eb7f4e4535
Peter Baumgartner <pete@lincolnloop.com>
2008-09-15 09:57:50 -0600

    changed name to prevent conflicts with other packages

--- locations/forms.py
+++
@@ -1,7 +0,0 @@
-from django import newforms as forms
-from models import Location
-        
-class LocationForm(forms.ModelForm):
-    class Meta:
-        model = Location
-        exclude = ('slug',)
